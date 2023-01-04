ba0acec2d73e334d0e91de69dac5112bc871a123
Gricel <gricel.dejesus@outlook.com>
2023-01-04 16:26:31 -0500

    login feature done

--- src/com/automation/git/GitPractice.java
+++ src/com/automation/git/GitPractice.java
@@ -5,7 +5,7 @@ public class GitPractice {
 	public static void main(String[] args) {
 	System.out.println("master");
 	System.out.println("login");
-	System.out.println("Login Feature Done");
+	System.out.println("login completed");
 
 	}
 
